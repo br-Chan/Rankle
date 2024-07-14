b1eed212a81f0355015ef4e49e3f816a69b698ad
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-07-15 11:29:33 +1200

    Remove state inputModules array and addInput Module function from StatModule, and add addButtonmodule function to App.jsx with inputModules prop passed into StatModules.

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,38 @@ import './App.css'
 import { StatModule } from './StatModule'
 import { ButtonModule } from './ButtonModule'
 
+function addButtonModule(index, queryText, numOfButtons, descending) {
+  // have an array of arrays of input modules (or a map?). Each array of input modules is then
+  // passed as a prop to the input module components.
+  setInputModules(
+    [
+      ...inputModules,
+      <ButtonModule queryText={queryText} numOfButtons={numOfButtons} descending={descending} />
+    ]
+  );
+}
+
 function createWordleStatModule() {
   return <StatModule gameName="Wordle" themeColor="#67a561" />
 }
 
 function App() {
+  const inputModules = [
+    <ButtonModule queryText="Query text" numOfButtons={6} descending={true} />,
+    <ButtonModule queryText="Select guesses made" numOfButtons="5" />
+  ];
 
   const statModules = [
     createWordleStatModule(),
     <StatModule gameName="Connections" themeColor="#bc70c4" />,
-    <StatModule gameName="Symble" themeColor="#f11415" />,
-    <StatModule gameName="Spotle" themeColor="#8370de" />,
-    <StatModule gameName="Dordle" themeColor="#fccc04" />,
-    <StatModule gameName="Dordle" themeColor="#f31064" />,
-    <StatModule gameName="Dordle" themeColor="#46013f" />,
-    <StatModule gameName="Bandle" themeColor="#f9deb2" />,
-    <StatModule gameName="Dordle" />,
-    <StatModule gameName="Dordle" />
+    <StatModule gameName="Symble" inputModules={inputModules} themeColor="#f11415" />,
+    <StatModule gameName="Spotle" inputModules={inputModules} themeColor="#8370de" />,
+    <StatModule gameName="Dordle" inputModules={inputModules} themeColor="#fccc04" />,
+    <StatModule gameName="Dordle" inputModules={inputModules} themeColor="#f31064" />,
+    <StatModule gameName="Dordle" inputModules={inputModules} themeColor="#46013f" />,
+    <StatModule gameName="Bandle" inputModules={inputModules} themeColor="#f9deb2" />,
+    <StatModule gameName="Dordle" inputModules={inputModules} />,
+    <StatModule gameName="Dordle" inputModules={inputModules} />
   ];
 
   // statModules[0].addInputModule("Hello", "5", "false");
--- src/StatModule.jsx
+++ src/StatModule.jsx
@@ -2,15 +2,13 @@ import { useState } from "react";
 
 import { ButtonModule } from "./ButtonModule";
 
-export function StatModule({ gameName, themeColor }) {
+export function StatModule({ gameName, inputModules, themeColor }) {
 
     // Array to contain all input modules in the stat module for this game.
     // const inputModules = [
     //     <ButtonModule queryText="Query text" numOfButtons={6} descending={true} />,
     //     <ButtonModule queryText="Select guesses made" numOfButtons="5" />
     // ];
-
-    const [inputModules, setInputModules] = useState([]);
 
     // Set the translucent colour for the background.
     let translucentThemeColor = themeColor + "25";
@@ -19,15 +17,6 @@ export function StatModule({ gameName, themeColor }) {
     const statModuleStyle = {
         borderColor: themeColor,
         backgroundColor: translucentThemeColor,
-    }
-
-    function addInputModule(queryText, numOfButtons, descending) {
-        setInputModules(
-            [
-                ...inputModules,
-                <ButtonModule queryText={queryText} numOfButtons={numOfButtons} descending={descending} />
-            ]
-        );
     }
 
     return (
