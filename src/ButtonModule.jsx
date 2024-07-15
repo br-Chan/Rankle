120eac4bac191ea031c3c122ba1dc4aff423b4f1
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-07-15 20:41:13 +1200

    Remove unused handleClick function from ButtonModule.jsx, merge creation of "X" button into for loop.

--- src/ButtonModule.jsx
+++ src/ButtonModule.jsx
@@ -1,34 +1,25 @@
 export function ButtonModule({ queryText, numOfButtons, descending }) {
     const buttons = [];
 
-    function handleClick(index) {
-
-    }
-
     // Clear the button text string.
     let buttonText = "";
 
     // Create each button with ascending or descending numbers.
-    for (let index = 0; index < numOfButtons; ++index) {
-        if (descending === true) {
+    for (let index = 0; index < numOfButtons + 1; ++index) {
+        if (index === numOfButtons) {
+            buttonText = "X";
+        } else if (descending === true) {
             buttonText = numOfButtons - index;
         } else {
             buttonText = index + 1;
         }
 
         buttons.push(
-            <button className="statModuleButton" key={index} value={index} onClick={() => handleClick(index)}>
+            <button className="statModuleButton" key={index} onClick={() => handleClick(index)}>
                 {buttonText}
             </button>
         );
     }
-
-    // Create an "X" button to be selected if the player failed at this game.
-    buttons.push(
-        <button className="statModuleButton" key="X" value="X" onClick={() => handleClick("X")}>
-            X
-        </button>
-    )
 
     return (
         <>
