d1baa890ebbfe21fce3cb272a0447677de10fa0f
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-08-22 13:18:13 +1200

    Remove Material Tailwind and form template code.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,6 @@
 import type { Config } from "tailwindcss";
 
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-module.exports = withMT({
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -18,5 +16,5 @@ module.exports = withMT({
     },
   },
   plugins: [],
-});
-// export default config;
+};
+export default config;
