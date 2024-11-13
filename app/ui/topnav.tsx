cfcb6db0365d6ef79818707194739cabde29ca2a
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-11-13 22:07:53 +1300

    feat: import hero icons from @heroicons/react and use them in top nav links.

--- app/ui/topnav.tsx
+++ app/ui/topnav.tsx
@@ -1,15 +1,14 @@
-"use client";
-
 import Link from "next/link";
 import { HoverTooltip } from "./hoverTooltip";
+import { PlusIcon, Squares2X2Icon } from "@heroicons/react/24/outline";
 
 const leftLinks = [];
 
 const rightLinks = [
-    { name: "All games", href: "/games" },
-    { name: "Create game", href: "/games/create" },
-    // {name:"", href:"",},
-    // {name:"", href:"",},
+    { name: "Add game", href: "/games/create", icon: PlusIcon },
+    { name: "All games", href: "/games", icon: Squares2X2Icon },
+    // {name:"", href:"", icon:,},
+    // {name:"", href:"", icon:,},
 ];
 
 /**
@@ -19,7 +18,7 @@ const rightLinks = [
  */
 export default function TopNav() {
     return (
-        <div className="flex justify-between items-center w-full">
+        <div className="flex justify-between items-center w-full px-5">
             <div className="w-96">
                 <button>hihi</button>
             </div>
@@ -30,13 +29,14 @@ export default function TopNav() {
 
             <div className="flex justify-end w-96">
                 {rightLinks.map((link) => {
+                    const LinkIcon = link.icon;
                     return (
                         <Link
                             key={link.name}
-                            className="relative mx-1 border-2 border-black bg-white rounded-md hover:bg-amber-200"
+                            className="relative mx-1 border-2 border-black rounded-lg hover:bg-amber-200"
                             href={link.href}
                         >
-                            <p className="peer">{link.name}</p>
+                            <LinkIcon className="peer w-8" />
                             <HoverTooltip key={`${link.name}`} tooltipText={link.name} />
                         </Link>
                     );
