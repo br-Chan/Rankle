f3d4109163b61a6b3ddaa870e73db9dc300d0799
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-11-13 14:46:14 +1300

    feat: revamp structure system of top nav so Rankle name stays in the middle while navigation buttons can be on the right or left sides of the bar.

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <html lang="en">
             <body className={inter.className}>
                 <main className="flex min-h-screen flex-col items-center justify-between p-24">
-                    <div className="fixed left-0 top-0 z-50 flex w-full justify-center bg-yellow-300 border-black border-2 rounded-3xl pb-1 pt-2 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit  lg:p-4 lg:dark:bg-zinc-800/30">
+                    <div className="fixed left-0 top-0 z-50 flex w-full justify-center bg-amber-300 border-black border-2 rounded-3xl pb-1 pt-2 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit  lg:p-4 lg:dark:bg-zinc-800/30">
                         <TopNav />
                     </div>
                     {children}
--- app/ui/topnav.tsx
+++ app/ui/topnav.tsx
@@ -1,13 +1,18 @@
 /**
  * The site navigation bar and app title located along the top of the screen.
- * 
+ *
  * @returns navigation bar
  */
 export default function TopNav() {
     return (
-        <div>
+        <div className="flex justify-between items-center w-full">
+            <div className="w-96">
+                <button>hihi</button>
+            </div>
             <h1 className="text-4xl text-black font-bold">RANKLE</h1>
-            {/* <button className="">hi</button> */}
+            <div className="flex justify-end w-96">
+                <button>hi</button>
+            </div>
         </div>
     );
 }
