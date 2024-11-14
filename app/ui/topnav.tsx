ae7380ae3f2d2fa608891a0e391c3daaacebed02
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-11-14 23:03:53 +1300

    feat: add hover tooltip to Rankle title (link to homepage) and make top nav link icons turn white upon hover.

--- app/ui/topnav.tsx
+++ app/ui/topnav.tsx
@@ -21,8 +21,9 @@ export default function TopNav() {
         <div className="flex justify-between items-center w-full px-5">
             <div className="w-96"></div>
 
-            <Link href={"/"}>
-                <h1 className="text-2xl text-black font-bold md:text-4xl">RANKLE</h1>
+            <Link className="relative" href={"/"}>
+                <h1 className="peer text-2xl text-black font-bold md:text-4xl">RANKLE</h1>
+                <HoverTooltip tooltipText="Home" delay="1000" />
             </Link>
 
             <div className="flex justify-end w-96">
@@ -31,7 +32,7 @@ export default function TopNav() {
                     return (
                         <Link
                             key={link.name}
-                            className="relative mx-1 border-2 border-black rounded-lg hover:bg-amber-500"
+                            className="relative mx-1 border-2 border-black rounded-lg transition-all hover:bg-amber-500 hover:text-white"
                             href={link.href}
                         >
                             <LinkIcon className="peer w-6 md:w-8" />
