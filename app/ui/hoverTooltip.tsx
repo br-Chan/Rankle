e6ded92fd21da88b00be0659f4d74e0cca4148dc
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-11-17 20:35:09 +1300

    fix: create dedicated function for determining the delay for hover tooltip, hopefully ensuring a custom input delay is properly used in the class name.

--- app/ui/hoverTooltip.tsx
+++ app/ui/hoverTooltip.tsx
@@ -1,3 +1,7 @@
+const getHoverDelayClassName = (delay?: string) => {
+    return delay ? `peer-hover:delay-1000` : "peer-hover:delay-300";
+}
+
 /**
  * A tooltip that is to be used in conjunction with a peer component that appears when the user
  * hovers over the peer component for a certain amount of time. displaying the text passed as a
@@ -10,7 +14,8 @@
  * @returns the tooltip
  */
 export const HoverTooltip = ({ tooltipText, delay }: { tooltipText: string; delay?: string }) => {
-    const hoverDelay = delay ? `peer-hover:delay-${delay}` : "peer-hover:delay-300";
+    const hoverDelay = getHoverDelayClassName(delay);
+
     return (
         <div
             className={`${hoverDelay} absolute left-1/2 transform -translate-x-1/2 w-max invisible opacity-0 peer-hover:visible peer-hover:opacity-100 bg-slate-700 text-sm text-white whitespace-pre-line rounded py-1 px-1 z-10 mt-1 mb-1`}
