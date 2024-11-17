6c804e7bd647bccf17140d9e75401684e97f6e11
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-11-17 22:20:27 +1300

    feat: implement loading text for the games page.

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -34,43 +34,51 @@ export default function Home() {
     return (
         <main className="">
             <h1 className="gap-2 text-center text-2xl font-black">GAMES</h1>
-            <p className="text-center">Add custom games or create your own!</p>
-            <div
-                className="
+            <p className="text-center mb-2">Add custom player-made games to your list!</p>
+
+            {/* Stat module panes */}
+            {statModulesData.length === 0 ? (
+                <p className="flex place-items-center px-16 py-1 rounded-xl font-mono text-2xl bg-amber-300">
+                    Loading all games...
+                </p>
+            ) : (
+                <div
+                    className="
                 pt-4 mb-32 grid gap-4 text-center
                 grid-cols-1 w-[288px]
                 md:grid-cols-2 md:w-[576px]
                 lg:grid-cols-3 lg:w-[864px]
                 2xl:grid-cols-4 2xl:w-[1152px]
             "
-            >
-                {statModulesData.map((item, index) => (
-                    <StatModulePane
-                        key={index}
-                        data={{
-                            id: item.id,
-                            gameName: item.gameName,
-                            inputModules: item.inputModules.map((item, index) => {
-                                return {
-                                    id: item.id,
-                                    statModuleId: item.statModuleId,
-                                    scoreIndex: index,
-                                    queryText: item.queryText,
-                                    buttonLabels: item.buttonLabels,
-                                    buttonScores: item.buttonScores,
-                                    enabled: true,
-                                    selectedButtonIndex: null,
-                                };
-                            }),
-                            themeColor: item.themeColor,
-                            enabled: true,
-                            hardModeEnabled: false,
-                            hardModeMultiplier: item.hardModeMultiplier,
-                        }}
-                        removeStatModuleFromStatModules={removeStatModuleFromStatModules}
-                    />
-                ))}
-            </div>
+                >
+                    {statModulesData.map((item, index) => (
+                        <StatModulePane
+                            key={index}
+                            data={{
+                                id: item.id,
+                                gameName: item.gameName,
+                                inputModules: item.inputModules.map((item, index) => {
+                                    return {
+                                        id: item.id,
+                                        statModuleId: item.statModuleId,
+                                        scoreIndex: index,
+                                        queryText: item.queryText,
+                                        buttonLabels: item.buttonLabels,
+                                        buttonScores: item.buttonScores,
+                                        enabled: true,
+                                        selectedButtonIndex: null,
+                                    };
+                                }),
+                                themeColor: item.themeColor,
+                                enabled: true,
+                                hardModeEnabled: false,
+                                hardModeMultiplier: item.hardModeMultiplier,
+                            }}
+                            removeStatModuleFromStatModules={removeStatModuleFromStatModules}
+                        />
+                    ))}
+                </div>
+            )}
         </main>
     );
 }
