f486048c939c36a5ef503439e312a574da2d99ef
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-11-18 19:48:35 +1300

    feat: add heroicons to replace add and del buttons in stat module pane (now UserPlusIcon and TrashIcon respectively).

--- app/ui/games/statModulePane.tsx
+++ app/ui/games/statModulePane.tsx
@@ -5,6 +5,7 @@ import { StatModuleData } from "../statModule";
 import { ButtonModulePane } from "./buttonModulePane";
 import { useAuth } from "@/app/hooks/useAuth";
 import { ThemedHoverComponent } from "../themedHoverComponent";
+import { UserPlusIcon, TrashIcon } from "@heroicons/react/24/solid";
 
 /**
  * Stat module pane for a single game, displaying all information about the stat module. The user
@@ -36,6 +37,42 @@ export const StatModulePane = ({
 
     return (
         <div className="relative transition-all duration-300 cursor-default">
+            {/* Title bar of the stat module pane*/}
+            <div
+                className="absolute flex justify-between items-center px-3 top-0 left-0 h-9 w-full font-bold opacity-100 transition-all duration-300 border-t-4 border-l-4 border-r-4 rounded-t-2xl"
+                style={{
+                    borderColor: `${data.themeColor}`,
+                }}
+            >
+                <ThemedHoverComponent
+                    className="border-2 border-black cursor-pointer flex items-center rounded-md"
+                    hoveredBackgroundColor="#4ade80" // text-green-400
+                >
+                    <button
+                        className="text-sm px-[2px] h-6 w-6"
+                        onClick={() => {
+                            addStatModuleToUser(user.uid, data.id);
+                        }}
+                    >
+                        <UserPlusIcon />
+                    </button>
+                </ThemedHoverComponent>
+                <h2 className="text-xl">{data.gameName}</h2>
+                <ThemedHoverComponent
+                    className="border-2 border-black cursor-pointer flex items-center rounded-md"
+                    hoveredBackgroundColor="#ef4444" // text-red-500
+                >
+                    <button
+                        className="text-sm px-[2px] h-6 w-6"
+                        onClick={() => {
+                            removeStatModuleFromStatModules(data.id);
+                        }}
+                    >
+                        <TrashIcon />
+                    </button>
+                </ThemedHoverComponent>
+            </div>
+
             {/* Body of the stat module pane*/}
             <div
                 className="h-full py-2 px-5 text-center transition-all duration-300 border-4 rounded-2xl"
@@ -68,45 +105,6 @@ export const StatModulePane = ({
                     {/* Input module panes */}
                     {inputModulePanes}
                 </div>
-            </div>
-
-            {/* Title bar of the stat module pane*/}
-            <div
-                className="absolute flex justify-between items-center px-3 top-0 left-0 h-9 w-full font-bold opacity-100 transition-all duration-300 border-t-4 border-l-4 border-r-4 rounded-t-2xl"
-                //                className="px-1 h-10 border-4 rounded-t-2xl"
-                style={{
-                    borderColor: `${data.themeColor}`,
-                }}
-            >
-                <ThemedHoverComponent
-                    className="border-2 border-black cursor-pointer flex items-center h-6 rounded-md"
-                    unhoveredBackgroundColor={data.themeColor}
-                    hoveredBackgroundColor="#4ade80" // text-green-400
-                >
-                    <button
-                        className="text-sm px-[2px]"
-                        onClick={() => {
-                            addStatModuleToUser(user.uid, data.id);
-                        }}
-                    >
-                        add
-                    </button>
-                </ThemedHoverComponent>
-                <h2 className="text-xl">{data.gameName}</h2>
-                <ThemedHoverComponent
-                    className="border-2 border-black cursor-pointer flex items-center h-6 rounded-md"
-                    unhoveredBackgroundColor={data.themeColor}
-                    hoveredBackgroundColor="#ef4444" // text-red-500
-                >
-                    <button
-                        className="text-sm px-[2px]"
-                        onClick={() => {
-                            removeStatModuleFromStatModules(data.id);
-                        }}
-                    >
-                        del
-                    </button>
-                </ThemedHoverComponent>
             </div>
         </div>
     );
