8b4275103025335cdc3eef9acba507d83162d00a
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-01-05 16:37:01 +1300

    feat: Use state variable mounted to render correct sun/moon icon when loading page

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
             <body
                 className={`${inter.className} bg-zinc-200 text-black antialiased dark:bg-zinc-800 dark:text-white`}
             >
-                <ThemeProvider attribute="class">
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
                     <main className="flex min-h-screen flex-col items-center justify-between p-24">
                         <div className="fixed left-0 top-0 z-50 flex w-full justify-center border-b-2 border-black bg-amber-300 pb-1 pt-2 dark:border-white dark:bg-amber-400 lg:p-4">
                             <TopNav />
--- app/ui/themeButton.tsx
+++ app/ui/themeButton.tsx
@@ -1,24 +1,35 @@
 "use client";
 
-import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import {
+    ComputerDesktopIcon,
+    MoonIcon,
+    SunIcon,
+} from "@heroicons/react/24/solid";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 const ThemeButton = () => {
+    const [mounted, setMounted] = useState(false);
     const { resolvedTheme, setTheme } = useTheme();
 
-    return (
+    useEffect(() => setMounted(true), []);
+
+    return mounted ? (
         <button
             type="button"
+            className="*:hover:bg-amber-500 *:hover:transition-colors"
             onClick={() => {
                 setTheme(resolvedTheme === "dark" ? "light" : "dark");
             }}
         >
             {resolvedTheme === "dark" ? (
-                <SunIcon className="m-2 w-5 rounded-full border-2 border-white p-1 text-white md:w-8" />
+                <SunIcon className="w-5 rounded-full border-2 border-white p-1 text-white md:w-8" />
             ) : (
-                <MoonIcon className="m-2 w-5 rounded-full border-2 border-black p-1 text-black md:w-8" />
+                <MoonIcon className="w-5 rounded-full border-2 border-black p-1 text-black md:w-8" />
             )}
         </button>
+    ) : (
+        <ComputerDesktopIcon className="h-5 w-5 rounded-full border-2 border-gray-500 p-1 text-gray-500 md:h-8 md:w-8" />
     );
 };
 
