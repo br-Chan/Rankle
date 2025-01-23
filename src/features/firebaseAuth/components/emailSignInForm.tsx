f8f08fe5d2ce05a4e263a3ac76e1aabc99b9d814
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-01-23 21:24:59 +1300

    style: Make sign in & sign up buttons disabled after clicking, and make fields only clickable when you click on the actual input only

--- src/features/firebaseAuth/components/emailSignInForm.tsx
+++ src/features/firebaseAuth/components/emailSignInForm.tsx
@@ -38,7 +38,7 @@ const EmailSignInForm = () => {
             onSubmit={handleEmailSignIn}
             className="flex flex-col items-center gap-4 rounded-2xl border-4 border-amber-400 bg-amber-400 bg-opacity-30 p-4"
         >
-            <label className="flex w-full flex-col items-center gap-1">
+            <label className="flex w-3/5 flex-col items-center gap-1">
                 <span className="font-bold">Email</span>
                 <input
                     type="email"
@@ -46,24 +46,25 @@ const EmailSignInForm = () => {
                     placeholder="rankler@rankmail.com"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    className="w-3/5 rounded-xl border-2 border-transparent bg-white bg-opacity-50 p-1 px-3 outline-none focus:border-amber-400"
+                    className="w-full rounded-xl border-2 border-transparent bg-white bg-opacity-50 p-1 px-3 outline-none focus:border-amber-400"
                 />
             </label>
 
-            <label className="flex w-full flex-col items-center gap-1">
+            <label className="flex w-3/5 flex-col items-center gap-1">
                 <span className="font-bold">Password</span>
                 <input
                     type="password"
                     name="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    className="w-3/5 rounded-xl border-2 border-transparent bg-white bg-opacity-50 p-1 px-3 outline-none focus:border-amber-400"
+                    className="w-full rounded-xl border-2 border-transparent bg-white bg-opacity-50 p-1 px-3 outline-none focus:border-amber-400"
                 />
             </label>
 
             <button
                 type="submit"
-                className="transition-colours w-fit rounded-lg bg-amber-400 px-4 py-2 text-black duration-300 hover:bg-amber-500"
+                disabled={isSigningIn}
+                className="transition-colours w-fit rounded-lg bg-amber-400 px-4 py-2 text-black duration-300 hover:bg-amber-500 disabled:pointer-events-none disabled:opacity-50 disabled:transition-none"
             >
                 Sign in
             </button>
--- src/features/firebaseAuth/components/signUpModal.tsx
+++ src/features/firebaseAuth/components/signUpModal.tsx
@@ -47,7 +47,7 @@ const SignUpModal = ({ onCancel }: { onCancel: () => void }) => {
                     onSubmit={handleEmailSignUp}
                     className="flex flex-col items-center gap-4 rounded-2xl border-4 border-amber-400 bg-amber-400 bg-opacity-30 p-4"
                 >
-                    <label className="flex w-full flex-col items-center gap-1">
+                    <label className="flex w-3/5 flex-col items-center gap-1">
                         <span className="font-bold">Email</span>
                         <input
                             type="email"
@@ -55,39 +55,40 @@ const SignUpModal = ({ onCancel }: { onCancel: () => void }) => {
                             placeholder="rankler@rankmail.com"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
-                            className="text-md w-3/5 rounded-xl border-2 border-transparent bg-white bg-opacity-50 p-1 px-3 outline-none focus:border-amber-400"
+                            className="text-md w-full rounded-xl border-2 border-transparent bg-white bg-opacity-50 p-1 px-3 outline-none focus:border-amber-400"
                         />
                     </label>
 
-                    <label className="flex w-full flex-col items-center gap-1">
+                    <label className="flex w-3/5 flex-col items-center gap-1">
                         <span className="font-bold">Password</span>
                         <input
                             type="password"
                             name="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
-                            className="text-md w-3/5 rounded-xl border-2 border-transparent bg-white bg-opacity-50 p-1 px-3 outline-none focus:border-amber-400"
+                            className="text-md w-full rounded-xl border-2 border-transparent bg-white bg-opacity-50 p-1 px-3 outline-none focus:border-amber-400"
                         />
                     </label>
 
-                    <label className="flex w-full flex-col items-center gap-1">
+                    <label className="flex w-3/5 flex-col items-center gap-1">
                         <span className="font-bold">Verify Password</span>
                         <input
                             type="password"
                             name="repeatedPassword"
                             value={password}
                             onChange={(e) => setRepeatedPassword(e.target.value)}
-                            className="text-md w-3/5 rounded-xl border-2 border-transparent bg-white bg-opacity-50 p-1 px-3 outline-none focus:border-amber-300"
+                            className="text-md w-full rounded-xl border-2 border-transparent bg-white bg-opacity-50 p-1 px-3 outline-none focus:border-amber-300"
                         />
                     </label>
 
                     <button
                         type="submit"
-                        className="transition-colours w-fit rounded-lg bg-amber-400 px-4 py-2 text-black duration-300 hover:bg-amber-500"
+                        disabled={isSigningUp}
+                        className="transition-colours w-fit rounded-lg bg-amber-400 px-4 py-2 text-black duration-300 hover:bg-amber-500 disabled:pointer-events-none disabled:opacity-50 disabled:transition-none"
                     >
                         Sign up
                     </button>
-                    
+
                     <button type="button" onClick={onCancel} className="text-sm hover:underline">
                         cancel
                     </button>
