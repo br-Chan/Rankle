5b851a0131a30f6bd2d8f8cd717f0920b42a2c8a
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-01-25 20:44:26 +1300

    refactor: create type for topnav link data and change padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,7 @@ export default function RootLayout({
             >
                 <AppProvider>
                     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-                        <div className="fixed left-0 top-0 z-50 flex w-full justify-center border-b-2 border-black bg-amber-300 pb-1 pt-2 lg:p-4 dark:border-white dark:bg-amber-500">
-                            <TopNav />
-                        </div>
+                        <TopNav />
                         {children}
                     </main>
                 </AppProvider>
--- src/features/topnav/components/topnav.tsx
+++ src/features/topnav/components/topnav.tsx
@@ -2,14 +2,14 @@
 
 import Link from "next/link";
 import { HoverTooltip } from "@/components/hoverTooltip";
-import { useAuth } from "@/features/firebaseAuth/hooks/useAuth";
 import { HiOutlinePlus, HiOutlineSquares2X2 } from "react-icons/hi2";
 import ThemeButton from "./themeButton";
 import AccountButton from "./accountButton";
+import { LinkData } from "../types/links";
 
-const leftLinks = [];
+const leftLinks: LinkData[] = [];
 
-const rightLinks = [
+const rightLinks: LinkData[] = [
     { name: "Add game", href: "/games/create", icon: HiOutlinePlus },
     { name: "All games", href: "/games", icon: HiOutlineSquares2X2 },
 ];
@@ -20,34 +20,34 @@ const rightLinks = [
  * @returns navigation bar
  */
 export default function TopNav() {
-    const { currentUser } = useAuth();
-
     return (
-        <div className="flex w-full items-center justify-between px-5">
-            <div className="flex flex-1">
-                <ThemeButton />
-            </div>
+        <div className="fixed left-0 top-0 z-50 flex w-full justify-center border-b-2 border-black bg-amber-300 p-2 lg:p-4 dark:border-white dark:bg-amber-500">
+            <div className="flex w-full items-center justify-between px-5">
+                <div className="flex flex-1">
+                    <ThemeButton />
+                </div>
 
-            <Link className="relative" href={"/"}>
-                <h1 className="peer text-2xl font-bold text-black md:text-4xl">RANKLE</h1>
-                <HoverTooltip tooltipText="Home" delay="1000" />
-            </Link>
+                <Link className="relative" href={"/"}>
+                    <h1 className="peer font-bold text-black text-4xl">RANKLE</h1>
+                    <HoverTooltip tooltipText="Home" delay="1000" />
+                </Link>
 
-            <div className="flex flex-1 justify-end">
-                {rightLinks.map((link) => {
-                    const LinkIcon = link.icon;
-                    return (
-                        <Link
-                            key={link.name}
-                            className="relative mx-1 rounded-lg border-2 border-black text-black transition-colors hover:bg-amber-500 hover:text-white dark:hover:bg-amber-300 dark:hover:text-black"
-                            href={link.href}
-                        >
-                            <LinkIcon className="peer h-8 w-8 dark:text-white" />
-                            <HoverTooltip key={link.name} tooltipText={link.name} />
-                        </Link>
-                    );
-                })}
-                <AccountButton />
+                <div className="flex flex-1 justify-end">
+                    {rightLinks.map((link) => {
+                        const LinkIcon = link.icon;
+                        return (
+                            <Link
+                                key={link.name}
+                                className="relative mx-1 rounded-lg border-2 border-black text-black transition-colors hover:bg-amber-500 hover:text-white dark:hover:bg-amber-300 dark:hover:text-black"
+                                href={link.href}
+                            >
+                                <LinkIcon className="peer h-8 w-8 dark:text-white" />
+                                <HoverTooltip key={link.name} tooltipText={link.name} />
+                            </Link>
+                        );
+                    })}
+                    <AccountButton />
+                </div>
             </div>
         </div>
     );
