c5d726b6f7506b1f37bf63d3cfcb95c6309fbaed
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-01-27 22:37:36 +1300

    feat: add account details and redo CSS for account page

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -10,12 +10,19 @@ const Account = () => {
     const router = useRouter();
 
     return (
-        <main className="flex w-[288px] flex-col justify-center text-center md:w-[576px]">
+        <>
             <h1 className="gap-2 text-center text-2xl font-black">ACCOUNT</h1>
             <p>Manage your account details and sign out!</p>
-            <div className="mt-10 flex flex-col gap-5">
-                <span className="rounded-xl bg-white p-2 dark:bg-zinc-900">
-                    Email: {currentUser ? currentUser.email : ""}
+
+            <div className="mt-6 flex w-full flex-col gap-4 md:w-[576px]">
+                <span className="rounded-xl bg-white p-2 text-center dark:bg-zinc-900">
+                    Name: {currentUser?.providerData[0].displayName ?? "none"}
+                </span>
+                <span className="rounded-xl bg-white p-2 text-center dark:bg-zinc-900">
+                    Email: {currentUser ? currentUser.email : "none"}
+                </span>
+                <span className="rounded-xl bg-white p-2 text-center dark:bg-zinc-900">
+                    Account type: {currentUser?.providerData[0].providerId}
                 </span>
                 <button
                     className="transition-colours rounded-full bg-amber-300 p-4 font-bold text-black duration-300 hover:bg-amber-500 hover:text-white"
@@ -27,7 +34,7 @@ const Account = () => {
                     SIGN OUT
                 </button>
             </div>
-        </main>
+        </>
     );
 };
 
