a9440ef6b559e4a86eae86c0ec90525c51a292c2
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-07-14 09:53:10 +1200

    refactor: input tooltip params into classname instead of in function

--- src/components/hoverTooltip.tsx
+++ src/components/hoverTooltip.tsx
@@ -1,7 +1,3 @@
-const getHoverDelayClassName = (delay?: string) => {
-    return delay ? `peer-hover:delay-1000` : "peer-hover:delay-300";
-};
-
 /**
  * A tooltip that is to be used in conjunction with a peer component that appears when the user
  * hovers over the peer component for a certain amount of time. displaying the text passed as a
@@ -15,12 +11,16 @@ const getHoverDelayClassName = (delay?: string) => {
  * @param props Component props
  * @returns the tooltip
  */
-export const HoverTooltip = ({ tooltipText, delay }: { tooltipText: string; delay?: string }) => {
-    const hoverDelay = getHoverDelayClassName(delay);
-
+export const HoverTooltip = ({
+    tooltipText,
+    delay = "300",
+}: {
+    tooltipText: string;
+    delay?: string;
+}) => {
     return (
         <div
-            className={`${hoverDelay} invisible absolute left-1/2 z-10 mb-1 mt-1 w-max -translate-x-1/2 transform whitespace-pre-line rounded bg-slate-800 px-1 py-1 text-sm text-white opacity-0 peer-hover:visible peer-hover:opacity-100`}
+            className={`peer-hover:delay-${delay} invisible absolute left-1/2 z-10 mb-1 mt-1 w-max -translate-x-1/2 transform whitespace-pre-line rounded bg-slate-800 px-1 py-1 text-sm text-white opacity-0 peer-hover:visible peer-hover:opacity-100`}
         >
             {tooltipText}
         </div>
