d45df9eadd285f14ace292dc7d2aa893a910ecac
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-07-22 12:12:02 +1200

    feat: add WIP pages for /about and /scores

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+const About = () => {
+    return (
+        <div className="mt-6 flex w-full max-w-md flex-col gap-4">
+            <Card>
+                <CardHeader>
+                    <CardTitle>Work in Progress!</CardTitle>
+                </CardHeader>
+                <CardContent>Page is under construction...</CardContent>
+            </Card>
+        </div>
+    );
+};
+
+export default About;
